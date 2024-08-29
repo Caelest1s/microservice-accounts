52c27accc90ff3b6d8624d38d1a411229a913fb1
Caelest1s <jefferson.crn@gmail.com>
2024-08-29 19:11:55 -0300

    Initial commit and installed dependency: Spring Web, H2, Spring JPA, Spring actuator, Spring DevTools, Lombok, Validation

---
+++ accounts/src/test/java/com/caelestis/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caelestis.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
