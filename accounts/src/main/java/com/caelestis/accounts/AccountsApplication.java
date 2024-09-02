77c059bc55e4017cb66ca477519da3d5d0bc022d
Caelest1s <jefferson.crn@gmail.com>
2024-09-02 12:21:11 -0300

    docs: Initial Info REST API Documentation

--- accounts/src/main/java/com/caelestis/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/caelestis/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.caelestis.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Accounts microservice bank integrated with many others microservice",
+				version = "v1",
+				contact = @Contact(
+						name = "Jefferson Celestino",
+						email = "contato@jeffersoncelestino.dev.br",
+						url = "https://www.linkedin.com/in/caelestis/"
+				),
+				license = @License(
+						name = "Licence: Apache 2.0",
+						url = "https://www.linkedin.com/in/caelestis/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Accounts microservice REST API External Documentation",
+				url = "https://www.linkedin.com/in/caelestis/"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
